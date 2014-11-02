b2d47f3fca55987dce4e2ff478a0e67c21471ffd
Sandeep Nautiyal <sandeep.gbpec2007@gmail.com>
2014-11-02 13:34:56 +0100

    Initial ProfessionalPA classes

---
+++ gen/com/gp/app/professionalpa/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gp.app.professionalpa;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/gp/app/professionalpa/layout/NotesLayoutManagerActivity.java
@@ -0,0 +1,39 @@
+package com.gp.app.professionalpa.layout;
+
+import com.gp.app.professionalpa.R;
+import com.gp.app.professionalpa.R.id;
+import com.gp.app.professionalpa.R.layout;
+import com.gp.app.professionalpa.R.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NotesLayoutManagerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_notes_layout_manager);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.notes_layout_manager, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
